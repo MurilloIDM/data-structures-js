64075a37eb5bc59a533d0ab6196f215f8958b3f3
MurilloIDM <murilloidm@gmail.com>
2021-02-24 08:11:59 -0300

    New: Conversão DEC>BIN uando stack

---
+++ challenges/binary-conversion.js
@@ -0,0 +1,24 @@
+const Stack = require('../stack/stack-array');
+
+function decimalToBinary(decimalNumber) {
+  const remStack = new Stack();
+
+  let number = decimalNumber;
+  let rem;
+  let binaryString = '';
+
+  while (number > 0) {
+    rem = Math.floor(number % 2);
+    remStack.push(rem);
+    number = Math.floor(number / 2);
+  }
+
+  while(!remStack.isEmpty()) {
+    binaryString += remStack.pop().toString();
+  }
+
+  return binaryString;
+}
+
+
+console.log(decimalToBinary(10));
